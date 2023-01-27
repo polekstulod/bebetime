15fe8e48e3544443ae90d1100153928b1b0d0ba3
James Paul Tulod <paultulod@pm.me>
2023-01-28 00:49:39 +0800

    Add mongoose and models

---
+++ backend/model/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const userModel = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://img.icons8.com/external-avatar-andi-nur-abdillah/64/null/external-avatar-avatarar-bussiness-avatar-andi-nur-abdillah-32.png",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model("User", userModel)
+
+module.exports = User
