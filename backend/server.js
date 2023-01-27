fb8dee7c67e8bde3956c6e71ee5d1a0642372a6a
James Paul Tulod <paultulod@pm.me>
2023-01-27 10:53:03 +0800

    Setup backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const { chats } = require("./data/data.js")
+
+const app = express()
+dotenv.config()
+
+app.get("/", (req, res) => {
+  res.send("API is running successfully")
+})
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats)
+})
+
+app.get("/api/chat/:id", (req, res) => {
+  const chat = chats.find(c => c._id === req.params.id)
+  res.send(chat)
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
