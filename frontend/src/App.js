6320bb756bcb9062256768f37751ee12401a6893
James Paul Tulod <paultulod@pm.me>
2023-01-27 20:54:31 +0800

    Connect frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import { Button } from "@chakra-ui/react"
+import { Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Chat from "./Pages/Chat"
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme="blue">Button</Button>
+      <Route path="/" component={Home} exact />
+      <Route path="/chats" component={Chat} />
     </div>
   )
 }
