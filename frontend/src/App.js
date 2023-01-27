44c006b43eb5d1fb9bfb6d4f3a549f548467a1b4
James Paul Tulod <paultulod@pm.me>
2023-01-27 19:41:48 +0800

    Setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  )
+}
+
+export default App
