c20905e95fc7d8ef0ca729cb2165d36af9c04070
James Paul Tulod <paultulod@pm.me>
2023-01-28 20:32:01 +0800

    Connect User Authentication to frontend

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -7,16 +7,70 @@ import {
   InputGroup,
   InputRightElement,
   Button,
+  useToast,
 } from "@chakra-ui/react"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
 
 const Login = () => {
   const [show, setShow] = useState(false)
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
+  const [loading, setLoading] = useState()
+  const toast = useToast()
+  const history = useHistory()
 
   const handleClick = () => setShow(!show)
 
-  const submitHandler = () => {}
+  const submitHandler = async () => {
+    setLoading(true)
+
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      })
+      setLoading(false)
+      return
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      )
+
+      toast({
+        title: "Login Successful.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      })
+
+      localStorage.setItem("userInfo", JSON.stringify(data))
+      setLoading(false)
+      history.push("/chats")
+    } catch (error) {
+      toast({
+        title: "Error Occurred.",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      })
+      setLoading(false)
+    }
+  }
 
   return (
     <VStack spacing="5px">
@@ -48,6 +102,7 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Login
       </Button>
--- frontend/src/components/authentication/Register.js
+++ frontend/src/components/authentication/Register.js
@@ -7,7 +7,10 @@ import {
   InputGroup,
   InputRightElement,
   Button,
+  useToast,
 } from "@chakra-ui/react"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
 
 const Register = () => {
   const [show, setShow] = useState(false)
@@ -15,10 +18,71 @@ const Register = () => {
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
   const [confirmPassword, setConfirmPassword] = useState()
+  const [loading, setLoading] = useState()
+  const toast = useToast()
+  const history = useHistory()
 
   const handleClick = () => setShow(!show)
 
-  const submitHandler = () => {}
+  const submitHandler = async () => {
+    setLoading(true)
+
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      })
+      setLoading(false)
+      return
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      })
+      setLoading(false)
+      return
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+
+      const { data } = await axios.post(
+        "/api/user",
+        { name, email, password, confirmPassword },
+        config
+      )
+
+      toast({
+        title: "Registration Successfully.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      })
+
+      localStorage.setItem("userInfo", JSON.stringify(data))
+      setLoading(false)
+      history.push("/chats")
+    } catch (error) {
+      toast({
+        title: "Registration Failed.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      })
+      setLoading(false)
+    }
+  }
 
   return (
     <VStack spacing="5px">
@@ -71,6 +135,7 @@ const Register = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Register
       </Button>
