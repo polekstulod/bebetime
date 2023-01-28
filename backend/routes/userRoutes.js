73e8af8b34bd7dc0bf8c2673950a7c56dbe2606e
James Paul Tulod <paultulod@pm.me>
2023-01-29 00:05:29 +0800

    Add authorization middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express")
-const { registerUser, authUser } = require("../controllers/userControllers")
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers")
+const protect = require("../middlewares/authMiddleware")
 
 const router = express.Router()
 
-router.route("/").post(registerUser)
+router.route("/").post(registerUser).get(protect, allUsers)
 router.post("/login", authUser)
 
 module.exports = router
