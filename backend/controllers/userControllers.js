97cc6cc879b062f4b8465b2d87821044f652a27b
James Paul Tulod <paultulod@pm.me>
2023-01-28 17:39:58 +0800

    Create user route and controller

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,41 @@
+const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel.js")
+const generateToken = require("../config/generateToken")
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error("Please fill in all fields")
+  }
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error("User already exists")
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid user data")
+  }
+})
+
+module.exports = { registerUser }
--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,19 @@ const dotenv = require("dotenv")
 const { chats } = require("./data/data.js")
 const connectDB = require("./config/db.js")
 const colors = require("colors")
+const userRoutes = require("./routes/userRoutes.js")
 
 const app = express()
 dotenv.config()
 connectDB()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("API is running successfully")
 })
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats)
-})
-
-app.get("/api/chat/:id", (req, res) => {
-  const chat = chats.find(c => c._id === req.params.id)
-  res.send(chat)
-})
+app.use("/api/user", userRoutes)
 
 const PORT = process.env.PORT || 4000
 
