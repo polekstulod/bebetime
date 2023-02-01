b32493144feba15b4c4ca2835bf814f712d60bd0
James Paul Tulod <paultulod@pm.me>
2023-02-01 23:36:04 +0800

    Add chat page header

---
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -0,0 +1,77 @@
+import React from "react"
+import {
+  Button,
+  IconButton,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react"
+import { ViewIcon } from "@chakra-ui/icons"
+
+const ProfileModal = ({ user, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton
+          display={{ base: "flex" }}
+          icon={<ViewIcon />}
+          onClick={onOpen}
+          aria-label="View Profile"
+        />
+      )}
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent h="410px">
+          <ModalHeader
+            fontSize="40px"
+            fontFamily="Average Sans"
+            display="flex"
+            justifyContent="center"
+          >
+            {user.name}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src={user.pic}
+              alt={user.name}
+            />
+            <Text
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily="Average Sans"
+            >
+              Email: {user.email}
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default ProfileModal
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,76 @@
 import React from "react"
+import {
+  Box,
+  Button,
+  Tooltip,
+  Text,
+  Menu,
+  MenuButton,
+  Avatar,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+} from "@chakra-ui/react"
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons"
+import { ChatState } from "../../context/ChatProvider"
+import ProfileModal from "./ProfileModal"
 
 const SideDrawer = () => {
-  return <div>Side Drawer</div>
+  /* const [search, setSearch] = useState("")
+  const [searchResults, setSearchResults] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [loadingChat, setLoadingChat] = useState()*/
+
+  const { user } = ChatState()
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="white"
+      w="100%"
+      p="5px 10px 5px 10px"
+      borderWidth="5px"
+    >
+      <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+        <Button variant="ghost">
+          <i className="fas fa-search"></i>
+          <Text d={{ base: "none", md: "flex" }} px="4">
+            Search User
+          </Text>
+        </Button>
+      </Tooltip>
+
+      <Text fontSize="2xl" fontFamily="Average Sans">
+        Bebe Time
+      </Text>
+      <div>
+        <Menu>
+          <MenuButton p={1}>
+            <BellIcon fontSize="2xl" m={1} />
+          </MenuButton>
+        </Menu>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            <Avatar
+              size="sm"
+              cursor="pointer"
+              name={user.name}
+              src={user.pic}
+            ></Avatar>
+          </MenuButton>
+          <MenuList>
+            <ProfileModal user={user}>
+              <MenuItem>My Profile</MenuItem>
+            </ProfileModal>
+            <MenuDivider />
+            <MenuItem>Logout</MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
+    </Box>
+  )
 }
 
 export default SideDrawer
