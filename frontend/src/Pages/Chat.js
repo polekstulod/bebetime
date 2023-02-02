d20b9b782e901e8eca03051886c310671a0cc62c
James Paul Tulod <paultulod@pm.me>
2023-02-02 09:21:46 +0800

    Move ChatBox and MyChat directory

--- frontend/src/Pages/Chat.js
+++ frontend/src/Pages/Chat.js
@@ -2,8 +2,8 @@ import React from "react"
 import { ChatState } from "../context/ChatProvider"
 import { Box } from "@chakra-ui/react"
 import SideDrawer from "../components/miscellaneous/SideDrawer"
-import MyChats from "../components/miscellaneous/MyChats"
-import ChatBox from "../components/miscellaneous/ChatBox"
+import MyChats from "../components/MyChats"
+import ChatBox from "../components/ChatBox"
 
 const Chat = () => {
   const { user } = ChatState()
