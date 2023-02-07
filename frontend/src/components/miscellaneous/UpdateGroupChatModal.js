29fea38cfcf0c40eb85dce6c308035a184e92442
James Paul Tulod <paultulod@pm.me>
2023-02-07 22:48:00 +0800

    Add rename group functionality

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -18,6 +18,7 @@ import {
 import { ViewIcon } from "@chakra-ui/icons"
 import { ChatState } from "../../context/ChatProvider"
 import UserBadgeItem from "../UserAvatar/UserBadgeItem"
+import axios from "axios"
 
 const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -33,7 +34,42 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
 
   const handleRemove = () => {}
 
-  const handleRename = () => {}
+  const handleRename = async () => {
+    if (!groupChatName) return
+
+    try {
+      setRenameLoading(true)
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }
+      const data = await axios.put(
+        "/api/chat/rename",
+        {
+          chatId: selectedChat._id,
+          chatName: groupChatName,
+        },
+        config
+      )
+
+      setSelectedChat(data)
+      setFetchAgain(!fetchAgain)
+      setRenameLoading(false)
+    } catch (e) {
+      toast({
+        title: "Error",
+        description: e.response.data.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      })
+      setRenameLoading(false)
+    }
+
+    setGroupChatName("")
+  }
 
   const handleSearch = () => {}
 
