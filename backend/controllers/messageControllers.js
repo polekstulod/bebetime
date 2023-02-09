138a7aef94069b103546c89772aa3094bd01aa42
James Paul Tulod <paultulod@pm.me>
2023-02-09 15:37:54 +0800

    Add sending message controller

---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,40 @@
+const asyncHandler = require("express-async-handler")
+const Message = require("../models/messageModel")
+const User = require("../models/userModel")
+const Chat = require("../models/chatModel")
+
+const sendMessage = asyncHandler(async (req, res) => {
+  const { chatId, content } = req.body
+
+  if (!chatId || !content) {
+    res.status(400)
+    throw new Error("Invalid request")
+  }
+
+  let newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  }
+
+  try {
+    let message = await Message.create(newMessage)
+
+    message = await message.populate("sender", "name pic")
+    message = await message.populate("chat")
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    })
+
+    await Chat.findByIdAndUpdate(req.body.chatId, {
+      latestMessage: message,
+    })
+
+    res.json(message)
+  } catch (e) {
+    res.status(400)
+    throw new Error(e.message)
+  }
+})
+module.exports = { sendMessage }
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const { sendMessage } = require("../controllers/messageControllers")
+const { protect } = require("../middlewares/authMiddleware")
 
 const router = express.Router()
 
-// router.route('/').post(protect, sendMessage)
+router.route("/").post(protect, sendMessage)
 // router.route('/:chatId').get(protect, allMessages)
 
 module.exports = router
